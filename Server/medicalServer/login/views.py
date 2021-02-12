cf84124824b351d2f6a4ce7ed34076a60060d3dc
jseidel00 <j.seidel@ufl.edu>
2021-02-11 19:50:12 -0500

    changed login response

--- Server/medicalServer/login/views.py
+++ Server/medicalServer/login/views.py
@@ -32,14 +32,19 @@ def auth_login(request):
         studies = request.user.study_set.all() #gets only the studies the user has accessed to:
         #print all studies this user can see
         s = list()
-        for study in studies:
-            #s = {"studyName" : "' + str(study.name) + '", "patientName" : "' + str(study.patient) + '", "studyID" : " + str(study._id) + " }
-            s.append({
-                "studyName" : str(study.name),
-                "patientName" : str(study.patient),
-                "studyID" : str(study.id),
-                "studyStatus": str(study.status)
-                })
+
+        s.append({
+            "id" : str(user.hash_id),
+            "type" : str(user.user_type)
+        })
+        # for study in studies:
+        #     #s = {"studyName" : "' + str(study.name) + '", "patientName" : "' + str(study.patient) + '", "studyID" : " + str(study._id) + " }
+        #     s.append({
+        #         "studyName" : str(study.name),
+        #         "patientName" : str(study.patient),
+        #         "studyID" : str(study.id),
+        #         "studyStatus": str(study.status)
+        #         })
                   
         return JsonResponse(s,safe=False)
     return HttpResponse(status=401)
