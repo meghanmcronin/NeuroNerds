c546185f36d620aef6925e8b14bd3db0be48a096
Riya Srivastava <srivastavar@ufl.edu>
2021-03-22 16:52:38 -0400

    nifti initial commit

--- app/src/main/java/com/example/nsgapp/InterfaceAPI.java
+++ app/src/main/java/com/example/nsgapp/InterfaceAPI.java
@@ -1,16 +1,23 @@
 package com.example.nsgapp;
 
 import java.util.List;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Streaming;
 
 public interface InterfaceAPI {
     @GET("studies")
     Call<List<Study>> getStudies();
+
+    @Streaming
+    @GET("studies?studyID=num")
+    Call<ResponseBody> downloadNifti();
 
     @POST("login")
     Call<LoginResponse> auth_login(@Header("Authorization") String authHeader, @Body LoginRequest loginRequest);
--- app/src/main/java/com/example/nsgapp/patient_list.java
+++ app/src/main/java/com/example/nsgapp/patient_list.java
@@ -16,11 +16,17 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -97,6 +103,27 @@ public class patient_list extends AppCompatActivity {
             }
         });
 
+        Call<ResponseBody> nifti = api.downloadNifti();
+        nifti.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Log.d("exists", "server contacted and has file");
+
+                    boolean writtenToDisk = writeResponseBodyToDisk(response.body());
+
+                    Log.d("downloaded", "file download was a success? " + writtenToDisk);
+                }
+                else {
+                    Log.d("failure", "server contact failed");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("error", "error");
+            }
+        });
 
         Button logout_button = (Button) findViewById(R.id.logout);
         logout_button.setOnClickListener(new View.OnClickListener() {
@@ -116,6 +143,55 @@ public class patient_list extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean writeResponseBodyToDisk(ResponseBody body) {
+        try {
+            File niftiFile = new File(getExternalFilesDir("studies?studyID=num") + File.separator + "Future Studio Icon.png");
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(niftiFile);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+
+                    fileSizeDownloaded += read;
+
+                    Log.d("downloaded", "file download: " + fileSizeDownloaded + " of " + fileSize);
+                }
+
+                outputStream.flush();
+
+                return true;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     private void logout() {
@@ -142,7 +218,8 @@ public class patient_list extends AppCompatActivity {
             }
         });
     }
-/*
+
+    /*
     public void back_to_login(View view)
     {
         String button_text;
